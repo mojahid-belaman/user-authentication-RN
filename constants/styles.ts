d0f0d75dbfae323c57657348e611b08b262e42d1
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-29 08:16:59 +0100

    create global stylwe

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,7 +1,17 @@
-export const Colors = {
+export const colors = {
   primary100: "#f9beda",
   primary500: "#c30b64",
   primary800: "#610440",
   error100: "#fcdcbf",
   error500: "#f37c13",
+};
+
+export const stackOptionsHeader = {
+  headerStyle: {
+    backgroundColor: colors.primary500,
+  },
+  headerTintColor: "white",
+  contentStyle: {
+    backgroundColor: colors.primary100,
+  },
 };
