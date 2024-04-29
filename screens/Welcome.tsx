20a8543bed61d2249833cf04481ce1c91ce9218a
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-29 08:15:17 +0100

    create screen Welcome

---
+++ screens/Welcome.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function WelcomeScreen() {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+      <Text>You Authenticated Successfully! </Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
