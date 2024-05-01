f6b188606bfc45dcf9319d914a42e99ca23e9e75
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-01 16:36:08 +0100

    update component LogInScreen handle states loading and error

--- screens/LogIn.tsx
+++ screens/LogIn.tsx
@@ -1,7 +1,28 @@
-import AuthContent from "../components/Auth/AuthContent";
+import { Alert } from "react-native";
+
+import AuthContent from "../components/auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { colors } from "../constants/styles";
+import useAuth from "../hooks/useAuth";
 
 function LogInScreen() {
-  return <AuthContent isLogin />;
+  const { loading, error, signInHandler } = useAuth();
+
+  if (loading) {
+    return (
+      <LoadingOverlay
+        loadingConfig={{ size: "large", color: colors.primary800 }}
+        message="Loggin You In..."
+      />
+    );
+  }
+
+  return (
+    <>
+      <AuthContent isLogin onAuthenticate={signInHandler} />
+      {error && Alert.alert("Authentication Failed!", error)}
+    </>
+  );
 }
 
 export default LogInScreen;
