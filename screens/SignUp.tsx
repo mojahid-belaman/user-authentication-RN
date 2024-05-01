b188c16f26f518a41924bfcbf115d9ae643e34e5
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-01 16:35:30 +0100

    update component SignUpScreen handle states loading and error

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,7 +1,28 @@
-import AuthContent from "../components/Auth/AuthContent";
+import { Alert } from "react-native";
+
+import AuthContent from "../components/auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { colors } from "../constants/styles";
+import useAuth from "../hooks/useAuth";
 
 function SignUpScreen() {
-  return <AuthContent />;
+  const { loading, error, signUpHandler } = useAuth();
+
+  if (loading) {
+    return (
+      <LoadingOverlay
+        loadingConfig={{ size: "large", color: colors.primary800 }}
+        message="Create User..."
+      />
+    );
+  }
+
+  return (
+    <>
+      <AuthContent onAuthenticate={signUpHandler} />
+      {error && Alert.alert("Authentication Failed!", error)}
+    </>
+  );
 }
 
 export default SignUpScreen;
