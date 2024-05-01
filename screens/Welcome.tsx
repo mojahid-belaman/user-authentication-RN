a83c69f71039c9699f8494834696f790c62540a1
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-01 16:47:29 +0100

    update component with addition of protect resources with token

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,12 +1,40 @@
-import { Text, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [data, setData] = useState("");
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch(
+      "https://expense-tracker-8e4e8-default-rtdb.firebaseio.com/message.json?auth=" +
+        token
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [token]);
+
   return (
-    <View>
-      <Text>Welcome!</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome!</Text>
       <Text>You Authenticated Successfully! </Text>
+      <Text>{data}</Text>
     </View>
   );
 }
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcome: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
